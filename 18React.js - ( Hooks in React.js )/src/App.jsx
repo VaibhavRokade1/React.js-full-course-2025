405a60065662e34882d211d4815db430399bda9a
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-16 11:31:27 -0930

    hooks understanding

---
+++ 18React.js - ( Hooks in React.js )/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Hooks In React.js</h1>
+      <ul>
+        <li>In old react version 16.8 we were using class component.</li>
+        <li>Now class based component are not much used in react.js</li>
+        <li>
+          So archieve state , life cycile , and other funtional components we us
+          Hooks
+        </li>
+      </ul>
+      <h3>Popular Hooks in React </h3>
+      <ol>
+        <li>useState</li>
+        <li>useEffect</li>
+        <li>useRef</li>
+        <li>useContext</li>
+        <li>useReduce</li>
+        <li>etc.</li>
+      </ol>
+    </>
+  );
+}
+
+export default App;
