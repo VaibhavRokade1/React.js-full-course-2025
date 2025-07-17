fa6785bdff04774ce27659475c129a1cb60a70a3
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-16 23:54:16 -0930

    react life cycile

--- 21React.js -( React Life Cycle  )/src/App.jsx
+++ 21React.js -( React Life Cycle  )/src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ShowSomething from "./ShowSomething";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  // mounting phase
+  useEffect(() => {
+    console.log("Mounting Phase Only....");
+  }, []);
+
+  // Update Phase
+  useEffect(() => {
+    console.log("Update Phase Only....");
+  }, [count]);
+
+  // unmounting
+  const [display, setDisplay] = useState(true);
+  useEffect(() => {
+    return () => {
+      console.log("Unmounting Only....");
+    };
+  }, [display]);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>App</h1>
+      <h2>React Life Cycile </h2>
+      <ol>
+        <li>Mounting</li>
+        <li>{`Updating ---->> (ErrorHandling)`}</li>
+        <li>Unmounting</li>
+      </ol>
+
+      <h1>Count {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+
+      <button
+        onClick={() => {
+          setDisplay(!display);
+        }}
+      >
+        Toggle
+      </button>
+
+      {display ? <ShowSomething /> : ""}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ 21React.js -( React Life Cycle  )/src/ShowSomething.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ShowSomething() {
+  return <h1>ShowSomething</h1>;
+}
+
+export default ShowSomething;
