0542fbe4ac946ba507d66968677ab5819401c8ee
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-17 00:07:26 -0930

    styling in react .js

---
+++ 22React.js - ( Style in React.js  )/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Types of Style in React.js</h1>
+      <ul>
+        <li>inline</li>
+        <li>internal</li>
+        <li>CSS</li>
+        <li>Styled Components</li>
+        <li>External Css Library/Framework</li>
+      </ul>
+    </>
+  );
+};
+
+export default App;
