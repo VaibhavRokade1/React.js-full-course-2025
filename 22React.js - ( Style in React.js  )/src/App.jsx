e9ccb457b3f686929e5acdf4c0582ab3c61589ed
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-17 17:06:54 -0930

    toggle style

--- 22React.js - ( Style in React.js  )/src/App.jsx
+++ 22React.js - ( Style in React.js  )/src/App.jsx
@@ -1,16 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [style, setStyle] = useState({
+    width: "200px",
+    borderRadius: "10px",
+    padding: "10px",
+  });
+
+  const handleTheme = (bgColor, textColor) => {
+    setStyle({ ...style, backgroundColor: bgColor, color: textColor });
+  };
   return (
     <>
-      <h1>Types of Style in React.js</h1>
+      {/* <h1>Types of Style in React.js</h1>
       <ul>
         <li>inline</li>
         <li>internal</li>
         <li>CSS</li>
         <li>Styled Components</li>
         <li>External Css Library/Framework</li>
-      </ul>
+      </ul> */}
+
+      <h1>Dynamic style and conditional style</h1>
+
+      <button
+        onClick={() => {
+          handleTheme("gray", "skyblue");
+        }}
+      >
+        Dark Theme
+      </button>
+      <button
+        onClick={() => {
+          handleTheme("white", "black");
+        }}
+      >
+        Default Theme
+      </button>
+
+      <div className="card" style={style}>
+        <img
+          src="https://avatars.githubusercontent.com/u/139482665?v=4"
+          alt=""
+          height={"200px"}
+          width={"200px"}
+          style={{
+            borderRadius: "10px",
+          }}
+        />
+        <h2>Vaibhav Rokade</h2>
+      </div>
     </>
   );
 };
