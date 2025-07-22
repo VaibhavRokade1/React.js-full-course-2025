b481507808e20850e1f24fc6412636835fc2e88f
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-21 23:43:26 -0930

    UnControlled Component Starting

---
+++ 26React.js -(Uncontroalled Components )/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>UnControlled Components</h1>
+      <ul>
+        <li>Without using states Thay working on from </li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
