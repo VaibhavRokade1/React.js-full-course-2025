e32ee66899035a925f401632a1261e659117741b
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-21 21:59:52 -0930

    ...

---
+++ 23React.js -( style over flow ( sideeffect ) handling )/src/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import style from "./style.module.css";
+import "./simpleStyle.css";
+function UserProfile() {
+  return (
+    <>
+      <h1 className={style.heading}>User Profile</h1>
+    </>
+  );
+}
+
+export default UserProfile;
