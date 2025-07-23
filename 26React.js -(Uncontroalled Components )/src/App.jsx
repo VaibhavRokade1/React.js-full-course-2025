214e1358bc20beaa79a874da12c49b9a98758b45
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-22 16:17:43 -0930

    Controalled Component

--- 26React.js -(Uncontroalled Components )/src/App.jsx
+++ 26React.js -(Uncontroalled Components )/src/App.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function App() {
+  let userName = useRef(null);
+  let userPassword = useRef(null);
+  const handleSubmit = () => {
+    console.log(userName.current.value, userPassword.current.value);
+  };
   return (
     <>
-      <h1>UnControlled Components</h1>
+      {/* <h1>UnControlled Components</h1>
       <ul>
         <li>Without using states Thay working on from </li>
-      </ul>
+      </ul> */}
+      <h1>Uncontrolled Components</h1>
+
+      <input type="text" ref={userName} placeholder="Enter Name" />
+      <br />
+      <br />
+      <input type="text" ref={userPassword} placeholder="Enter Password" />
+      <br />
+      <br />
+      <button onClick={handleSubmit}>Submit Data</button>
     </>
   );
 }
