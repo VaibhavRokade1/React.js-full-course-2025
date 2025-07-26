aedcb12153839f38d40d0ebf197e293d1138966c
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-26 00:17:35 -0930

    Derived Sate

---
+++ 32React.js -( Derived States in React.js )/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1>Derived State</h1>
+      <ul>
+        <li>
+          State that is calculated or derived form the onthor state value or
+          props within your components.
+        </li>
+        <li>Derived state can be a variable</li>
+        <li>No Need to extra state only variable and constants are enough</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
