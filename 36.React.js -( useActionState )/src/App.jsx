ea9c597774f38f4d1a3056d8aefeb95698953206
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-29 13:02:12 -0930

    useActionState

--- 36.React.js -( useActionState )/src/App.jsx
+++ 36.React.js -( useActionState )/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useActionState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // in from data paramiter we can access all from Data
+  const formHandler = async (previousData, FromData) => {
+    const name = FromData.get("name");
+    const password = FromData.get("password");
+
+    await new Promise((res) => setTimeout(res, 2000));
+
+    if (name && password) {
+      return { massage: "Data Submited...!", name, password };
+    } else {
+      return {
+        error: "Failed to send data...!",
+        name,
+        password,
+      };
+    }
+  };
+
+  const [data, action, pending] = useActionState(formHandler, undefined);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>useActionState in React.js</h1>
+      <form action={action}>
+        <input type="text" placeholder="Enter a name" name="name" />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="Enter a Password"
+          name="password"
+          autoComplete="true"
+        />
+        <br />
+        <br />
+        <button disabled={pending}>
+          {pending ? "Submitting..." : "Submit"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <br />
+        {data?.error && <span style={{ color: "red" }}>{data?.error}</span>}
+        {data?.massage && (
+          <span style={{ color: "green" }}>{data?.massage}</span>
+        )}
+        <h1>Name : {data?.name}</h1>
+        <h1>password : {data?.password}</h1>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
