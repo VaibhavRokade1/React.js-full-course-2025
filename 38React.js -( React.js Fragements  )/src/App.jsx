5cef6ac94ef1ffe92e96f51fc54df04127d9d4b6
Vaibhav Rokade <rokadevaibhav04@.com>
2025-07-30 16:27:14 -0930

    React.js Fragments

---
+++ 38React.js -( React.js Fragements  )/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1>React Fragements </h1>
+      <ul>
+        <li>
+          without using reactfragement we can not return multiple html elements
+        </li>
+      </ul>
+
+      <ol>
+        <li>
+          react Fragements can not create any self elements (div) it is alogical
+          entity.
+        </li>
+      </ol>
+    </>
+  );
+}
+
+// ex.
+
+// function name() {
+//    return {name : "vaibhav"} { price :" dfjkfkshf"};
+// }
+
+export default App;
