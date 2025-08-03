8cf5639d5e3a55cb9ca603e02395b1f268ce5916
Vaibhav Rokade <rokadevaibhav04@.com>
2025-08-02 17:22:58 -0930

    React Router

---
+++ 42React.js - ( React-Router )/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1>React-Router Library</h1>
+      <p>
+        React Router is a standard routing library for React that enables
+        navigation between pages (components) in a single-page application (SPA)
+        without reloading the page.
+      </p>
+    </>
+  );
+}
+
+export default App;
