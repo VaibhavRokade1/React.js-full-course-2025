ac0e4f20e25a8dd0043a38bd1fa0557565b0913b
Vaibhav Rokade <rokadevaibhav04@.com>
2025-08-06 13:37:38 -0930

    Layout and indexing in react.js

--- 45React.js -( To Do App )/src/App.jsx
+++ 45React.js -( To Do App )/src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.js
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [toDos, setToDoS] = useState([]);
+
+  const handleToDo = (e) => {
+    e.preventDefault();
+    if (!toDos.includes(input)) {
+      setToDoS([...toDos, input]);
+      setInput("");
+    } else {
+      alert("To Do Already Exists...!!!");
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>To Do App</h1>
+      <form onSubmit={handleToDo}>
+        <div className="main">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            required
+          />
+          <button type="submit">Add</button>
+        </div>
+        <Ullist toDos={toDos} setToDoS={setToDoS} />
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+export function Ullist({ toDos, setToDoS }) {
+  const handleRemove = (index) => {
+    const finalData = toDos.filter((_, i) => i !== index);
+    setToDoS(finalData);
+  };
+
+  const [status, setStatus] = useState(false);
+  const handleComplete = () => {
+    setStatus(!status);
+  };
+
+  return (
+    <>
+      <ul>
+        {toDos.map((todo, index) => {
+          return (
+            <li
+              key={index}
+              className={status ? "done" : ""}
+              onClick={handleComplete}
+            >
+              <span>
+                {index + 1 + "."}&nbsp;
+                {todo}
+              </span>
+              <span className="remove" onClick={() => handleRemove(index)}>
+                &nbsp;X
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
