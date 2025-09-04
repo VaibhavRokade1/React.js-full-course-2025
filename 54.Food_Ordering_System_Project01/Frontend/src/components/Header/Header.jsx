a2f89b9854fb0fc01632137d15d5ac092a680dda
Vaibhav Rokade <rokadevaibhav04@gmail.com>
2025-09-04 23:22:04 +0530

    Home page is working on process

---
+++ 54.Food_Ordering_System_Project01/Frontend/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi dolore
+          delectus ullam fugiat porro ratione voluptatem ea aliquid minima
+          numquam? Amet inventore ducimus neque quisquam ipsum accusamus officia
+          aliquid nihil.
+        </p>
+        <button>View menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
