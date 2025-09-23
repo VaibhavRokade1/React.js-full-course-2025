91f25cea7ec769b54669e949ed9ab15441883284
Vaibhav Rokade <rokadevaibhav04@gmail.com>
2025-09-23 07:24:11 +0530

    struture

--- 56MyViteApp/src/App.jsx
+++ 56MyViteApp/src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Home />
+      <About />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
