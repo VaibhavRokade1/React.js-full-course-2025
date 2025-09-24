a85089ba08eb0d9307a6ea4b7875abdebc279fcd
Vaibhav Rokade <rokadevaibhav04@gmail.com>
2025-09-24 07:50:30 +0530

    save changes

--- 56MyViteApp/src/App.jsx
+++ 56MyViteApp/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Footer from "./components/Footer/Footer";
+import { Contact } from "./Pages/Contact";
 
 function App() {
   return (
     <>
       <Home />
       <About />
+      <Contact />
       <Footer />
     </>
   );
